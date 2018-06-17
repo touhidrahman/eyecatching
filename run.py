63b600341c72102f0d79c337acc369a0f2d3f5fa
Touhid Rahman <touhidrahman1987@gmail.com>
2018-06-18 01:44:12 +0200

    Implement different opacity

--- run.py
+++ run.py
@@ -2,46 +2,51 @@ import subprocess, os, sys, shutil
 from PIL import Image
 import imagehash
 
-def getImageSize(filename):
+
+def get_image_size(filename):
     """
     Get image size
     """
     img = Image.open(filename)
-    size = wd, ht = img.size
+    size = img.size
     del img
     return size
 
-def getScreenshot(browser, url, imageSize, imageName):
+
+def get_screenshot(browser, url, imageSize, imageName):
     """
     Get screenshot of the given webpage
     """
-    defaultName = "screenshot.png"
-    
-    def firefox(url, imageSize):
-        windowSize = "--window-size={0}".format(imageSize['width'] + 10)
+    default_name = "screenshot.png"
+
+    def firefox(url, image_size):
+        windowSize = "--window-size={0}".format(image_size['width'] + 10)
         subprocess.call(["firefox", "-screenshot", windowSize, url])
         # remove the scrolbar 
-        chopFromRight(defaultName, 10)
-    
-    def chrome(url, imageSize):
-        windowSize = "--window-size={0},{1}".format(
-            imageSize['width'], imageSize['height']
+        remove_pixels_right(default_name, 10)
+
+    def chrome(url, image_size):
+        window_size = "--window-size={0},{1}".format(
+            image_size['width'], image_size['height']
         )
         subprocess.call(["/opt/google/chrome/chrome",
-                        "--headless",
-                        "--hide-scrollbars",
-                        windowSize,
-                        "--screenshot",
-                        url])
-        
-    switcher = {'firefox': firefox, 'chrome': chrome}
+                         "--headless",
+                         "--hide-scrollbars",
+                         window_size,
+                         "--screenshot",
+                         url])
+
+    switcher = {
+        'firefox': firefox,
+        'chrome': chrome
+    }
     switcher[browser](url, imageSize)
     # rename picture
-    subprocess.call(["mv", defaultName, imageName])
+    subprocess.call(["mv", default_name, imageName])
     print("Saved screenshot from {0} with name {1}".format(browser, imageName))
 
 
-def chopFromRight(image, pixels):
+def remove_pixels_right(image, pixels):
     """
     Subtract given pixels from right side of the image 
     and replace the original file.
@@ -52,169 +57,166 @@ def chopFromRight(image, pixels):
     print("Removed {0} pixels from the right side of image {1}".format(pixels, image))
 
 
-def extendImage(image, factor):
+def extend_image(image: str, factor: int):
     """
     Extend the image to be equally divisible by factor
+    :type image: str
+    :type factor: int
     """
-    wd, ht = getImageSize(image)
-    exWd = factor - (wd % factor)
-    exHt = factor - (ht % factor)
+    wd, ht = get_image_size(image)
+    ex_wd = factor - (wd % factor)
+    ex_ht = factor - (ht % factor)
 
-    if (exHt != factor):
-        exH = "0x{0}".format(exHt)
-        subprocess.call(["convert", image, "-gravity", "south", "-splice", exH, image])
-        print("Extended {0} pixels at the bottom of image {1}".format(exHt, image))
+    if ex_ht != factor:
+        ex_ht_str = "0x{0}".format(ex_ht)
+        subprocess.call(["convert", image, "-gravity", "south", "-splice", ex_ht_str, image])
+        print("Extended {0} pixels at the bottom of image {1}".format(ex_ht, image))
 
-    if (exWd != factor):
-        exW = "{0}x0".format(exWd)
-        subprocess.call(["convert", image, "-gravity", "east", "-splice", exW, image])
-        print("Extended {0} pixels at the right of image {1}".format(exWd, image))
-    
+    if ex_wd != factor:
+        ex_wd_str = "{0}x0".format(ex_wd)
+        subprocess.call(["convert", image, "-gravity", "east", "-splice", ex_wd_str, image])
+        print("Extended {0} pixels at the right of image {1}".format(ex_wd, image))
 
-    
-def tileImage(filename, edge):
+
+def tile_image(filename: str, edge: int):
     """
     Slice image into tiles with meaningful naming
     and move them into Prefixed directory
     """
-    wd, ht = getImageSize(filename)
+    wd, ht = get_image_size(filename)
     img = Image.open(filename)
     prefix = filename.split('.')[0]
     extension = filename.split('.')[1]
     counter = 0
 
-    if (os.path.isdir(prefix)):
+    if os.path.isdir(prefix):
         shutil.rmtree(prefix)
+
     os.mkdir(prefix)
+
     print("Slicing image {0} into {1} x {1} pixel tiles".format(filename, edge))
+
     for x in range(0, wd, edge):
         for y in range(0, ht, edge):
-            croppedImg = img.crop((x, y, x+edge, y+edge))
-            cFilename = "{0}/{0}_{1}_{2}.{3}".format(
-                    prefix, x, y, extension
-                )
-            croppedImg.save(cFilename)
+            cropped_img = img.crop((x, y, x + edge, y + edge))
+            cropped_filename = "{0}/{0}_{1}_{2}.{3}".format(
+                prefix, x, y, extension
+            )  # this format helps saving inside subdirectory
+            cropped_img.save(cropped_filename)
             counter = counter + 1
-            del croppedImg
+            del cropped_img
     print("Generated {0} images in directory {1}".format(counter, prefix))
 
 
-def blendImage(baseImage, opacity):
+def mark_image(image: str, opacity: float):
     """
     Mask image with a color
     """
-    size = getImageSize(baseImage)
-    image1 = Image.new("RGB", size, "salmon")
-    image2 = Image.open(baseImage).convert("RGB")
-    blended = Image.blend(image1, image2, opacity)
-    blended.save(baseImage)
+    img_bottom = Image.open(image).convert("RGB")
+    img_top = Image.new("RGB", img_bottom.size, "salmon")
+    blended = Image.blend(img_top, img_bottom, opacity)
+    blended.save(image)
 
-    del image1, image2, blended
+    del img_bottom, img_top, blended
 
 
-def compareTiles(basedir, comparedir):
+def compare_tiles(ref_dir, compare_dir):
     """
     Compares tiles and marks different tiles in comparing directory
     """
-    pathA = os.getcwd() + "/" + basedir
-    pathB = os.getcwd() + "/" + comparedir
+    path = os.getcwd() + "/" + compare_dir
 
-    for tile in os.listdir(pathA):
-        filenameA = basedir + "/" + tile
-        filenameB = comparedir + "/" + tile.replace(basedir, comparedir)
-        hashDiff = getHashDiff(filenameA, filenameB)
-    
+    # clean up directory, only image files should exist
 
+    for tile in os.listdir(path):
+        tile_ref_img = ref_dir + "/" + tile.replace(compare_dir, ref_dir)
+        tile_com_img = compare_dir + "/" + tile
+        hash_diff = get_hash_diff(tile_ref_img, tile_com_img)
 
-def remakeImage(refImage, compareImage):
-    size = getImageSize(refImage)
-    canvas = Image.new("RGB", size, "white")
-    dirA = refImage.split('.')[0]
-    dirB = compareImage.split('.')[0]
-    pathA = os.getcwd() + "/" + dirA
-    pathB = os.getcwd() + "/" + dirB
+        # print((tile_ref_img, tile_com_img))
 
-    for filename in os.listdir(pathA):
-        filenameA = dirA + "/" + filename
-        filenameB = dirB + "/" + filename.replace(dirA, dirB)
-        # print(filenameA, filenameB)
-        # print(getHashDiff(filenameA, filenameB))
-        hashDiff = getHashDiff(filenameA, filenameB)
-
-        if (hashDiff <= 30):
-            blendImage(filenameA, 0.1)
-        elif (hashDiff <= 40):
-            blendImage(filenameA, 0.2)
-        elif (hashDiff <= 50):
-            blendImage(filenameA, 0.3)
-        elif (hashDiff <= 60):
-            blendImage(filenameA, 0.4)
-        elif (hashDiff <= 70):
-            blendImage(filenameA, 0.5)
+        if hash_diff >= 30 and hash_diff < 39:
+            mark_image(tile_com_img, 0.1)
+        elif hash_diff >= 40 and hash_diff < 49:
+            mark_image(tile_com_img, 0.2)
+        elif hash_diff >= 50 and hash_diff < 59:
+            mark_image(tile_com_img, 0.3)
+        elif hash_diff >= 60 and hash_diff < 69:
+            mark_image(tile_com_img, 0.4)
         else:
-            blendImage(filenameA, 0.6)
+            mark_image(tile_com_img, 0.5)
 
+
+def remake_image(ref_img, compare_img):
+    print("Marking visual differences from reference image...")
+    size = get_image_size(ref_img)
+    canvas = Image.new("RGB", size, "white")
+    dir_ref = ref_img.split('.')[0]
+    dir_com = compare_img.split('.')[0]
+    path = os.getcwd() + "/" + dir_com
+
+    compare_tiles(dir_ref, dir_com)
+
+    for filename in os.listdir(path):
+        img_tile = dir_com + "/" + filename
         prefix, x, yy = filename.split('_')
         y = yy.split(".")[0]
-        img = Image.open(filenameA)
+        img = Image.open(img_tile)
+        # print(filename)
         canvas.paste(img, (int(x), int(y)))
         del img
 
-    canvas.save("output.png")
+    saving_name = "output" + "_" + compare_img
+    canvas.save(saving_name)
+    print("Resulted file saved as {0}".format(saving_name))
 
 
-def makeImageFromTiles(dirname):
-    size = getImageSize(dirname + '.png')
-    canvas = Image.new("RGB", size, "white")
-    path = os.getcwd() + '/' + dirname
-
-    for filename in os.listdir(path):
-        tilename = dirname + '/' + filename
-
-def getHashDiff(image1, image2):
+def get_hash_diff(image1, image2):
     img1 = Image.open(image1)
     img2 = Image.open(image2)
-    hash1 = imagehash.average_hash(img1)
-    hash2 = imagehash.average_hash(img2)
-    print((hash1, hash2))
+    hash1 = imagehash.dhash(img1)
+    hash2 = imagehash.dhash(img2)
     del img1, img2
     return abs(hash1 - hash2)
 
+
 def main():
     print('Working....')
-    
-    url = "http://angular.io"
-    factor = 10
-    imageSize = {'width': 1280}
-    imageChrome = "A.png"
-    imageFirefox = "B.png"
-    
-    getScreenshot('firefox', url, imageSize, imageFirefox)
-    
-    # get viewport height from firefox image
-    imageSize['height'] = Image.open(imageFirefox).size[1]
-    
-    getScreenshot('chrome', url, imageSize, imageChrome)
 
-    print("Resulted image size is {0} x {1}".format(imageSize['width'], imageSize['height']))
-    
-    # entend images to cut precisely
-    extendImage(imageChrome, factor)
-    extendImage(imageFirefox, factor)
-    
+    url = "http://angular.io"
+    factor = 20
+    image_size = {'width': 1280}
+    image_chrome = "A.png"
+    image_firefox = "B.png"
+
+    get_screenshot('firefox', url, image_size, image_firefox)
+
+    # get viewport height from firefox image
+    image_size['height'] = Image.open(image_firefox).size[1]
+
+    get_screenshot('chrome', url, image_size, image_chrome)
+
+    print("Resulted image size is {0} x {1}".format(image_size['width'], image_size['height']))
+
+    # extend images to cut precisely
+    extend_image(image_chrome, factor)
+    extend_image(image_firefox, factor)
+
     # slice to tiles
-    tileImage(imageChrome, factor)
-    tileImage(imageFirefox, factor)
+    tile_image(image_chrome, factor)
+    tile_image(image_firefox, factor)
 
     # join slices
-    remakeImage(imageChrome, imageFirefox)
+    remake_image(image_chrome, image_firefox)
+
+    print("Done.")
 
 
 def test():
     # tileImage("A.png", 400)
-    remakeImage("A.png", "B.png")
+    remake_image("A.png", "B.png")
 
 
-if (__name__ == "__main__"):
-    # main()
-    test()
+if __name__ == "__main__":
+    main()
+    # test()
