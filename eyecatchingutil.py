16e1d613da09dcfd77372748cc4c423c5a2276bc
Touhid Rahman <touhidrahman1987@gmail.com>
2018-07-14 00:06:39 +0200

    Recursive crop working

--- eyecatchingutil.py
+++ eyecatchingutil.py
@@ -23,17 +23,17 @@ class MetaImage:
         self.height = self.image.size[1]
         self.ext = self.imagename.split(".")[1]
 
+        self.actual_coords = Coordinates(0, 0, self.width, self.height)
+        self.virtual_coords = Coordinates(0, 0, self.width, self.height)
+
         namebits = self.imagename.split("_") # A_0_0_100_100_.png
+        # following coords are relative to original image
         if len(namebits) > 1:
-            self.left       = int(namebits[1])
-            self.top        = int(namebits[2])
-            self.right      = int(namebits[3])
-            self.bottom     = int(namebits[4])
-        else:
-            self.left = 0
-            self.top = 0
-            self.right = self.width
-            self.bottom = self.height
+            _l = int(namebits[1])
+            _t = int(namebits[2])
+            _r = int(namebits[3])
+            _b = int(namebits[4])
+            self.virtual_coords = Coordinates(_l, _t, _r, _b)
 
     def a_hash(self):
         return imagehash.average_hash(self.image)
@@ -50,41 +50,10 @@ class MetaImage:
         return prefix
 
     def coordinates(self):
-        return (
-            self.left, self.top,
-            self.right, self.bottom
-        )
+        return self.actual_coords.as_tuple()
 
-# TODO: some problem here
-    def _even(self, length):
-        x = int(length / 2)
-        if x % 2 != 0:
-            x -= 1
-        return x
-
-    def left_half(self):
-        return (
-            self.left,                  self.top,
-            self._even(self.width),     self.height
-        )
-
-    def right_half(self):
-        return (
-            self._even(self.width),     self.top,
-            self.width,                 self.height
-        )
-
-    def top_half(self):
-        return (
-            self.left,                  self.top,
-            self.width,                 self._even(self.height)
-        )
-
-    def bottom_half(self):
-        return (
-            self.left,                  self._even(self.height), 
-            self.width,                 self.height
-        )
+    def relative_coordinates(self):
+        return self.virtual_coords.as_tuple()
 
     def is_landscape(self):
         return self.width > self.height
@@ -92,26 +61,14 @@ class MetaImage:
     def is_potrait(self):
         return self.height > self.width
 
-    def first_half(self):
-        if self.is_landscape():
-            return self.left_half()
-        if self.is_potrait():
-            return self.top_half()
-
-    def second_half(self):
-        if self.is_landscape():
-            return self.right_half()
-        if self.is_potrait():
-            return self.bottom_half()
-
     def format_name(self, coords = None):
         if (coords != None):
-            (t, l, b, r) = coords
+            (l, t, r, b) = coords
         else:
-            (t, l, b, r) = (self.top, self.left, self.bottom, self.right)
+            (l, t, r, b) = self.virtual_coords.as_tuple()
 
         return "{0}_{1}_{2}_{3}_{4}_.{5}".format(
-            self.prefix, t, l, b, r, self.ext
+            self.prefix, l, t, r, b, self.ext
         )
 
     def save(self):
@@ -121,42 +78,54 @@ class MetaImage:
         del self.image
         os.remove(self.imagename)
 
-    def crop(self, coordinates):
-        img = self.image.crop(coordinates)
-        filename = self.format_name(coordinates)
-        if (os.path.isfile(filename)):
+    def crop(self, crop_coords, naming_coords):
+        img = self.image.crop(crop_coords)
+        filename = self.format_name(naming_coords)
+        if os.path.isfile(filename):
             os.remove(filename)
         img.save(filename)
 
     def save_top_half(self):
-        self.crop(self.top_half())
+        actual = self.actual_coords.top_half()
+        virtual = self.virtual_coords.top_half()
+        self.crop(actual, virtual)
 
     def save_bottom_half(self):
-        self.crop(self.bottom_half())
+        actual = self.actual_coords.bottom_half()
+        virtual = self.virtual_coords.bottom_half()
+        self.crop(actual, virtual)
 
     def save_left_half(self):
-        self.crop(self.left_half())
+        actual = self.actual_coords.left_half()
+        virtual = self.virtual_coords.left_half()
+        self.crop(actual, virtual)
 
     def save_right_half(self):
-        self.crop(self.right_half())
+        actual = self.actual_coords.right_half()
+        virtual = self.virtual_coords.right_half()
+        self.crop(actual, virtual)
 
     def save_first_half(self):
-        self.crop(self.first_half())
+        actual = self.actual_coords.first_half()
+        virtual = self.virtual_coords.first_half()
+        self.crop(actual, virtual)
 
     def save_second_half(self):
-        self.crop(self.second_half())
+        actual = self.actual_coords.second_half()
+        virtual = self.virtual_coords.second_half()
+        self.crop(actual, virtual)
 
     def divide(self):
         bigger = self.height
-        if self.width > self.height:
+        if self.width >= self.height:
             bigger = self.width
 
-        if bigger > MetaImage.LIMIT * 2:
+        if bigger >= MetaImage.LIMIT * 2:
             self.save_first_half()
             self.save_second_half()
             return (
-                self.format_name(self.first_half()),
-                self.format_name(self.second_half())
+                self.format_name(self.virtual_coords.first_half()),
+                self.format_name(self.virtual_coords.second_half())
                 )
         print("exit divide")
         return (None, None)
@@ -204,3 +173,92 @@ class ImageComparator:
 
     def hamming_diff_d_hash(self):
         return abs(self.image1.d_hash() - self.image2.d_hash())
+
+
+
+class Coordinates:
+    def __init__(self, l, t, r, b):
+        self.l = l
+        self.t = t
+        self.r = r
+        self.b = b
+        mid_x = int(abs(t - b) / 2)
+        mid_y = int(abs(l - r) / 2)
+        if mid_x % 2 == 0:
+            self.mid_x = mid_x
+        else:
+            self.mid_x = mid_x + 1
+        if mid_y % 2 == 0:
+            self.mid_y = mid_y
+        else:
+            self.mid_y = mid_y + 1
+
+    def as_tuple(self):
+        return (
+            self.l, self.t,
+            self.r, self.b
+        )
+
+    def shift_horizontally(self, pixels: int):
+        self.l += pixels
+        self.r += pixels
+        return (
+            self.l, self.t,
+            self.r, self.b
+        )
+
+    def shift_vertically(self, pixels: int):
+        self.t += pixels
+        self.b += pixels
+        return (
+            self.l, self.t,
+            self.r, self.b 
+        )
+
+    def left_half(self):
+        return (
+            self.l,     self.t,
+            self.mid_x, self.b
+        )
+
+    def right_half(self):
+        return (
+            self.mid_x, self.t,
+            self.r,     self.b
+        )
+
+    def top_half(self):
+        return (
+            self.l,     self.t,
+            self.r,     self.mid_y
+        )
+
+    def bottom_half(self):
+        return (
+            self.l,     self.mid_y,
+            self.r,     self.b
+        )
+
+    # def distance_x(self, c: Coordinates):
+    #     return abs(self.l - c.l)
+
+    # def distance_y(self, c: Coordinates):
+    #     return abs(self.t - c.t)
+
+    def is_potrait(self):
+        return abs(self.r - self.l) <= abs(self.b - self.t)
+
+    def is_landscape(self):
+        return abs(self.r - self.l) >= abs(self.b - self.t)
+
+    def first_half(self):
+        if self.is_landscape():
+            return self.left_half()
+        if self.is_potrait():
+            return self.top_half()
+
+    def second_half(self):
+        if self.is_landscape():
+            return self.right_half()
+        if self.is_potrait():
+            return self.bottom_half()
--- testpil.py
+++ testpil.py
@@ -9,24 +9,25 @@ def main():
     print('Working....')
     image1 = MetaImage("b.jpg")
     image2 = MetaImage("a.jpg")
-    # image1.image.show()
-    # image2.image.show()
+    # image1.divide()
 
     recursive_check("a.jpg", "b.jpg")
 
 
 
-def recursive_check(image1, image2):
+def recursive_check(image1, image2, count = 0):
+    count += 1
+    print(count)
     img1 = MetaImage(image1)
     img2 = MetaImage(image2)
     c = ImageComparator(img1, img2)
     if c.hamming_diff() > 0:
         (a1, a2) = img1.divide()
         (b1, b2) = img2.divide()
-        if a1 != None or b1 != None:
-            recursive_check(a1, b1)
-        if a2 != None or b2 != None:
-            recursive_check(a2, b2)
+        if a1 != None and b1 != None:
+            recursive_check(a1, b1, count)
+        if a2 != None and b2 != None:
+            recursive_check(a2, b2, count)
     else:
         img2.delete()
 
